fabb0674411a1200282664f13360a04f30d28fd3
damqu44 <damian.charazka1@gmail.com>
2024-11-21 22:24:33 +0100

    update

--- src/server.js
+++ src/server.js
@@ -53,10 +53,6 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!")
 })
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"))
-})
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
